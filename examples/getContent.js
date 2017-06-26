2d3d8368980808e8b2b3515b7b00813ef188b834
3hdeng <3hdeng@gmail.com>
2017-06-26 12:01:50 +0000

    try node-github/examples

--- examples/getContent.js
+++ examples/getContent.js
@@ -1,4 +1,21 @@
 "use strict";
+var util=require("util");
+var path=require("path");
+
+var args = process.argv.slice(2);
+var owner="elfscript";
+var repo="nodegit";
+
+if(args.length >=1){
+	        repo= args[0];
+}
+
+if(args.length >=2){
+	        owner= args[1];
+}
+
+console.log(util.format("to getContent()  from %s owned by %s",repo,owner));
+
 
 var Client = require("./../lib/index");
 
@@ -7,8 +24,8 @@ var github = new Client({
 });
 
 github.repos.getContent({
-    owner: "mikedeboer",
-    repo: "node-github",
+    owner: owner,
+    repo: repo,
     path: ""
 }, function(err, res) {
     console.log(err, res);
--- examples/getRepos.js
+++ examples/getRepos.js
@@ -1,7 +1,19 @@
 "use strict";
+var util=require("util");
+var path=require("path");
+
+var args = process.argv.slice(2);
+var owner="elfscript";
+
+if(args.length >=1){
+	                owner= args[0];
+}
+
+console.log(util.format("to getRepos()  from  %s",owner));
+
 
 var Client = require("./../lib/index");
-var testAuth = require("./../testAuth.json");
+var testAuth = require("./../test_token.json");
 
 var github = new Client({
     debug: true
